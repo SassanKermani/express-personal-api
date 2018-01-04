5fef80c32e4a6e44d0e2f9e17730d9f4deeadd2f
SassanKermani <SassanKermani2@gmail.com>
2018-01-03 17:35:50 -0700

    first commit

--- models/index.js
+++ models/index.js
@@ -1,7 +1,6 @@
 var mongoose = require("mongoose");
-mongoose.connect( process.env.MONGODB_URI || 
-                  process.env.MONGOLAB_URI || 
-                  process.env.MONGOHQ_URL || 
+mongoose.connect( 
+				  process.env.MONGODB_URI ||
                   "mongodb://localhost/personal-api");
 
 // module.exports.Campsite = require("./campsite.js.example");
